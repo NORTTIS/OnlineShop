e71c0dea374257b093a610c113a8045de3a280ec
JustMing <nqm061204@gmail.com>
2024-11-06 15:59:26 +0700

    view products list

--- OnlineShop/Models/OnlineShopContext.cs
+++ OnlineShop/Models/OnlineShopContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace OnlineShop.Models;
 
@@ -26,9 +28,15 @@ public partial class OnlineShopContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=MSI\\SQLEXPRESS;database=OnlineShop;uid=sa;pwd=123;TrustServerCertificate=True;");
-
+    {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        IConfiguration config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+        var strConn = config["ConnectionStrings:MyDatabase"];
+        optionsBuilder.UseSqlServer(strConn);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
--- OnlineShop/ProductManage.xaml.cs
+++ OnlineShop/ProductManage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,48 @@ namespace OnlineShop
     /// </summary>
     public partial class ProductManage : Window
     {
+        OnlineShopContext context = new OnlineShopContext();
         public ProductManage()
         {
             InitializeComponent();
+            loadProducts();
         }
+
+        public void loadProducts()
+        {
+            var list = context.Products.Select(c => new
+            {
+                Name = c.Name,
+                Price = c.Price,
+                Quantity = c.QuantityInStock,
+                Category = c.Category.Name
+            }).Distinct().ToList();
+            lstView.ItemsSource = list;
+        }
+
+        private void lbDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedProd = lstView.SelectedItem as dynamic;
+            if (selectedProd != null)
+            {
+                tbName.Text = selectedProd.Name.ToString();
+            }
+        }
+
     }
 }
